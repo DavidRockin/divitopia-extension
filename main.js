4e38c090f21dd6f642a95b7c6a0e0a33bb0a1f03
Dave <david.tkachuk@hotmail.com>
2017-11-27 19:11:56 -0500

    just a cleanup, style adjustments

--- main.js
+++ main.js
@@ -6,7 +6,6 @@
 	function fetchPrices() {
 		var xhttp = new XMLHttpRequest();
 		xhttp.onreadystatechange = function() {
-			console.log(this);
 			if (this.readyState == 4 && this.status == 200) {
 				prices = JSON.parse(this.responseText);
 			}
@@ -31,15 +30,19 @@
 
 		var tooltip  = document.createElement('div');
 		Object.assign(tooltip.style, {
-			background : '#333',
+			background : '#dddd',
 			border     : '1px solid #999',
 			position   : 'absolute',
 			'z-index'  : 99999999999,
 			top        : (e.clientY+1) + 'px',
-			left       : (e.clientX+1) + 'px'
+			left       : (e.clientX+1) + 'px',
+			'text-align' : 'center',
+			padding      : '9px 16px'
 		});
 		tooltip.id = 'diviTooltip';
-		tooltip.innerHTML = btc + " BTC = $ " + price.toFixed(3) + " USD";
+		tooltip.innerHTML = '<h2 style="font-size:14pt;margin-top:0px;padding-top:0px;">The Divi Project</h2>';
+		tooltip.innerHTML += btc + " BTC = $ " + price.toFixed(3) + " USD";
+		tooltip.innerHTML += '<h4 style="font-size:12pt;">Crypto Made Easy</h4>';
 		document.body.appendChild(tooltip);
 	}
 
@@ -48,7 +51,6 @@
 	setInterval(() => {
 		document.querySelectorAll("tr.currencyData-tradepair td:nth-child(3), table#buyorders tbody tr td, table#sellorders tbody tr td").forEach((d) => {
 			registerEvent(d, 'mouseenter', (e) => {
-				console.log(e);
 				showTooltip(e);
 			});
 		});
