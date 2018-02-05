77f2fa838ac8bcdd62c4d66e763a0efd16b99cda
Dave <david.tkachuk@hotmail.com>
2018-02-05 17:35:39 -0500

    cryptopia balance special trigger

--- src/triggers.js
+++ src/triggers.js
@@ -1,5 +1,19 @@
+/**
+ * Cryptopia Balance special trigger
+ *
+ * A quick and hacky way to detect the active currency
+ * selected on the balances page
+ *
+ * @param {Element} e
+ */
 function cryptopiaBalances(e) {
-	return {
-		curr : 'DIVX'
+	try {
+		var curr = e.target.parentNode.childNodes[3].childNodes[3].innerText;
+		if (null !== curr)
+			return {
+				curr : curr.trim()
+			}
+	} catch (e) {
 	}
+	return {};
 }
