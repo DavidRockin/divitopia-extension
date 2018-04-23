1958036df7088721df7a3d3086d1f380c338c16c
Dave <david.tkachuk@hotmail.com>
2018-04-22 23:27:10 -0400

    dont open changelog for minor release

--- src/background/browser.js
+++ src/background/browser.js
@@ -1,13 +1,30 @@
-getBrowser().runtime.onInstalled.addListener((info) => {
-	url = '/' + (info.reason === 'update' ? 'install-v' + getAppVersion() : 'install') + '.html';
+/**
+ * Divitopia Browser Scripts
+ *
+ * For now this will just open up a changelog on the main website for when the
+ * extension gets installed for the first time or is updated.
+ */
+
+getBrowser().runtime.onInstalled.addListener(info => {
+
+	// if a minor version, don't open tabs
+	if (getAppVersion().split('.').length > 2) {
+		return;
+	}
+
+	const url = divitopiaURL + '/install' + 
+		(info.reason === 'update'
+			? '-v' + getAppVersion() // an updated version
+			: '' // first installation
+		) + '.html'
+	;
 
 	// we should only open a tab upon installation or update
 	if (info.reason !== 'install' && info.reason !== 'update') {
 		return;
 	}
 
-	getBrowser().tabs.create({
-		url : divitopiaURL + url
-	})
+	// this will open the tab
+	getBrowser().tabs.create({ url })
 
-});
+})
